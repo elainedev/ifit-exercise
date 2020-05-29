29bc8a5397be084a5e9b3a6b5a67d1896be7fa32
Elaine Wang <elainewang.cal@gmail.com>
2020-05-29 00:25:21 -0700

    Initial files

---
+++ src/ifit-page.jsx
@@ -0,0 +1,20 @@
+class IFitPage extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+	}
+
+	render() {
+
+		return (
+			<div>ice cream</div>
+		)
+	}
+
+}
+
+const domContainer = document.querySelector("#ifit-page");
+ReactDOM.render(<IFitPage />, domContainer);
